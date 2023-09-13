e6fa2e58c1d82ac912a2621221ca3c7dafc193b2
Carol Silva <contatocarolinebs@gmail.com>
2023-09-13 10:48:30 -0300

    adiciona arquivo de html, css e script

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-hamburguer').addEventListener('click', () => {
+  document.querySelector('.dp-none').classList.toggle('nav-bar-mobile');
+  document.querySelector('.transparent').classList.toggle('dp-block');
+})
